5a631e5cf0c0558cbdcdadf5004fafe2d3ce3eb9
PrettyMisaka <123646696+PrettyMisaka@users.noreply.github.com>
2023-01-27 12:55:56 +0800

    1.1
    稍微改进了算法，添加了过渡动画和新元素出现时的动画，对一个元素和总的对象进行优化，整合了之前独立的函数以便进行更好的调用
    improvement algorithm , add animation and add function to object

--- index.js
+++ index.js
@@ -23,6 +23,50 @@ let totalData = new function () {
     this.arr = blockArrInit();
     this.valuearr = ValueArrInit();
     this.ablePlace = 16;
+    this.animationShow = (mod) => {
+        for (let x = 0; x < 4; x++) {
+            for (let y = 0; y < 4; y++) {
+                this.arr[y][x].animationSet(mod);
+            }
+        }
+    }
+    this.gameUpdate = () => {
+        let gameVacancy = 0, isBreak = false;
+        for (let x = 0; x < 4; x++) {
+            for (let y = 0; y < 4; y++) {
+                if (this.valuearr[y][x] === 0) gameVacancy++;
+                this.arr[y][x].isNew = false;
+            }
+        }
+        let randomNum = parseInt(getRandomArbitrary(1, gameVacancy + 1));
+        console.log(randomNum + '|' + gameVacancy);
+        gameVacancy = 0;
+        for (let x = 0; x < 4; x++) {
+            for (let y = 0; y < 4; y++) {
+                if (this.valuearr[y][x] === 0) gameVacancy++;
+                if (gameVacancy === randomNum) {
+                    this.valuearr[y][x] = 2;
+                    this.arr[y][x].isNew = true;
+                    isBreak = true;
+                    break;
+                }
+            }
+            if (isBreak) break;
+        }
+    };
+    this.totalReFlash = function () {
+        for (let x = 0; x < 4; x++) {
+            for (let y = 0; y < 4; y++) {
+                this.arr[y][x].value = totalData.valuearr[y][x];
+                this.arr[y][x].blockColorFlash();
+                this.arr[y][x].showValue();
+                if (totalData.arr[y][x].isNew) {
+                    // this.arr[y][x].el.style.border = 'solid 2px black';
+                    this.arr[y][x].updateAnimationSet();
+                } else this.arr[y][x].el.style.border = 'none';
+            }
+        }
+    }
 }
 let ISMobile = isMobile();
 if (ISMobile) {
@@ -42,8 +86,8 @@ document.addEventListener('selectstart', function (e) {
     //禁止右键选择
 })
 console.log(isMobile());
-gameUpdate();
-totalReFlash();
+totalData.gameUpdate();
+totalData.totalReFlash();
 eventReg();
 
 function BlockInit(x, y) {
@@ -64,6 +108,27 @@ function BlockInit(x, y) {
     this.showValue = () => {
         if (this.value === 0) this.el.innerHTML = ''
         else this.el.innerHTML = this.value;
+    }
+    this.animationSet = function (mod) {
+        this.el.style.transform = 'none';
+        this.el.style.transition = 'ease .1s';
+        if (this.value !== 0) {
+            let animationStrong = '8px';
+            let transformBearing = ['0,-' + animationStrong, '0,' + animationStrong, '-' + animationStrong + ',0', animationStrong + ',0'];
+            this.el.style.transform = 'translate(' + transformBearing[mod] + ')';
+            setTimeout(() => {
+                this.el.style.transform = 'translate(0,0)';
+            }, 100)
+        }
+    }
+    this.updateAnimationSet = function () {
+        this.el.style.transform = 'none';
+        this.el.style.transform = 'scale(0,0)';
+        this.el.style.transform = 'scale(115%,115%)';
+        this.el.style.transition = 'ease 0.4s';
+        setTimeout(() => {
+            this.el.style.transform = 'scale(100%,100%)';
+        }, 400)
     }
 }
 
@@ -132,12 +197,11 @@ function eventReg() {
                 if (journeyY.value < 0) tmpBearing = 'UP';
                 else tmpBearing = 'DOWN';
             }
+            totalData.animationShow(actionBearing[tmpBearing]);
             judgeAndAdd(actionBearing[tmpBearing]);
-            judgeAndAdd(actionBearing[tmpBearing]);
-            judgeAndAdd(actionBearing[tmpBearing]);
-            console.log(totalData.valuearr);
-            gameUpdate();
-            totalReFlash();
+            // console.log(totalData.valuearr);
+            totalData.gameUpdate();
+            totalData.totalReFlash();
         }
     } else {
         bodyEl.ontouchstart = function (e) {
@@ -168,149 +232,91 @@ function eventReg() {
                 if (journeyY.value < 0) tmpBearing = 'UP';
                 else tmpBearing = 'DOWN';
             }
+            totalData.animationShow(actionBearing[tmpBearing]);
             judgeAndAdd(actionBearing[tmpBearing]);
-            judgeAndAdd(actionBearing[tmpBearing]);
-            judgeAndAdd(actionBearing[tmpBearing]);
-            console.log(totalData.valuearr);
-            gameUpdate();
-            totalReFlash();
+            // console.log(totalData.valuearr);
+            totalData.gameUpdate();
+            totalData.totalReFlash();
         }
     }
 
 
-}
-
-function gameUpdate() {
-    let gameVacancy = 0, isBreak = false;
-    for (let x = 0; x < 4; x++) {
-        for (let y = 0; y < 4; y++) {
-            if (totalData.valuearr[y][x] === 0) gameVacancy++;
-            totalData.arr[y][x].isNew = false;
-        }
-    }
-    let randomNum = parseInt(getRandomArbitrary(1, gameVacancy + 1));
-    console.log(randomNum + '|' + gameVacancy);
-    gameVacancy = 0;
-    for (let x = 0; x < 4; x++) {
-        for (let y = 0; y < 4; y++) {
-            if (totalData.valuearr[y][x] === 0) gameVacancy++;
-            if (gameVacancy === randomNum) {
-                totalData.valuearr[y][x] = 2;
-                totalData.arr[y][x].isNew = true;
-                isBreak = true;
-                break;
-            }
-        }
-        if (isBreak) break;
-    }
 }
 
 function getRandomArbitrary(min, max) {// 大于等于 min小于max。
     return Math.random() * (max - min) + min;
 }
 
-function totalReFlash() {
-    for (let x = 0; x < 4; x++) {
-        for (let y = 0; y < 4; y++) {
-            totalData.arr[y][x].value = totalData.valuearr[y][x];
-            totalData.arr[y][x].blockColorFlash();
-            totalData.arr[y][x].showValue();
-            if (totalData.arr[y][x].isNew) totalData.arr[y][x].el.style.border = 'solid 2px black';
-            else totalData.arr[y][x].el.style.border = 'none';
-        }
-    }
-}
-
 
 function judgeAndAdd(mod) {
     if (mod === 0) {//向上 只有y要换
         for (let x = 0; x < 4; x++) {
-            for (let y = 3; y >= 0; y--) {
-                let atPresentY = y, atPresentX = x;
-                for (let i = y - 1; i >= 0; i--) {
-                    let el = totalData.valuearr[atPresentY][atPresentX];
+            for (let y = 0; y < 4; y++) {
+                for (let i = 1; i < (4 - y); i++) {
+                    let el = totalData.valuearr[i - 1][x];
                     let nextEl = totalData.valuearr[i][x];
-                    if (nextEl === 0) {//为空 往上移动 互换
-                        totalData.valuearr[i][x] = el;
-                        totalData.valuearr[atPresentY][atPresentX] = 0;
-                        atPresentY = i;
+                    if (el === 0) {//为空 往上移动 互换
+                        totalData.valuearr[i][x] = 0;
+                        totalData.valuearr[i - 1][x] = nextEl;
                         continue;
                     }
-                    if (nextEl !== el) {
-                        break;
-                    } else if (nextEl === el) {
-                        totalData.valuearr[i][x] = el + nextEl;
-                        totalData.valuearr[atPresentY][atPresentX] = 0;
-                        atPresentY = i;
+                    if (nextEl === el) {
+                        totalData.valuearr[i][x] = 0;
+                        totalData.valuearr[i - 1][x] = el + nextEl;
                     }
                 }
             }
         }
     } else if (mod === 1) {//向下 只有y要换
         for (let x = 0; x < 4; x++) {
-            for (let y = 0; y < 4; y++) {
-                let atPresentY = y, atPresentX = x;
-                for (let i = y + 1; i < 4; i++) {
-                    let el = totalData.valuearr[atPresentY][atPresentX];
+            for (let y = 3; y >= 0; y--) {
+                for (let i = 2; i >= 0; i--) {
+                    let el = totalData.valuearr[i + 1][x];
                     let nextEl = totalData.valuearr[i][x];
-                    if (nextEl === 0) {//为空 往下移动 互换
-                        totalData.valuearr[i][x] = el;
-                        totalData.valuearr[atPresentY][atPresentX] = 0;
-                        atPresentY = i;
+                    if (el === 0) {//为空 拉取上值 互换
+                        totalData.valuearr[i + 1][x] = nextEl;
+                        totalData.valuearr[i][x] = 0;
                         continue;
                     }
-                    if (nextEl !== el) {
-                        break;
-                    } else if (nextEl === el) {
-                        totalData.valuearr[i][x] = el + nextEl;
-                        totalData.valuearr[atPresentY][atPresentX] = 0;
-                        atPresentY = i;
+                    if (nextEl === el) {
+                        totalData.valuearr[i + 1][x] = el + nextEl;
+                        totalData.valuearr[i][x] = 0;
                     }
                 }
             }
         }
     } else if (mod === 2) {//向左 只有x要换
         for (let y = 0; y < 4; y++) {
-            for (let x = 3; x >= 0; x--) {
-                let atPresentY = y, atPresentX = x;
-                for (let i = x - 1; i >= 0; i--) {
-                    let el = totalData.valuearr[atPresentY][atPresentX];
+            for (let x = 0; x < 4; x++) {
+                for (let i = 1; i < 4; i++) {
+                    let el = totalData.valuearr[y][i - 1];
                     let nextEl = totalData.valuearr[y][i];
-                    if (nextEl === 0) {//为空 往下移动 互换
+                    if (el === 0) {//为空 往下移动 互换
                         totalData.valuearr[y][i] = el;
-                        totalData.valuearr[atPresentY][atPresentX] = 0;
-                        atPresentX = i;
+                        totalData.valuearr[y][i - 1] = nextEl;
                         continue;
                     }
-                    if (nextEl !== el) {
-                        break;
-                    } else if (nextEl === el) {
-                        totalData.valuearr[y][i] = el + nextEl;
-                        totalData.valuearr[atPresentY][atPresentX] = 0;
-                        atPresentX = i;
+                    if (nextEl === el) {
+                        totalData.valuearr[y][i - 1] = el + nextEl;
+                        totalData.valuearr[y][i] = 0;
                     }
                 }
             }
         }
     } else if (mod === 3) {//向右 只有x要换
         for (let y = 0; y < 4; y++) {
-            for (let x = 0; x < 4; x++) {
-                let atPresentY = y, atPresentX = x;
-                for (let i = x + 1; i < 4; i++) {
-                    let el = totalData.valuearr[atPresentY][atPresentX];
+            for (let x = 3; x >= 0; x--) {
+                for (let i = 2; i >= 0; i--) {
+                    let el = totalData.valuearr[y][i + 1];
                     let nextEl = totalData.valuearr[y][i];
-                    if (nextEl === 0) {//为空 往下移动 互换
+                    if (el === 0) {//为空 往下移动 互换
                         totalData.valuearr[y][i] = el;
-                        totalData.valuearr[atPresentY][atPresentX] = 0;
-                        atPresentX = i;
+                        totalData.valuearr[y][i + 1] = nextEl;
                         continue;
                     }
-                    if (nextEl !== el) {
-                        break;
-                    } else if (nextEl === el) {
-                        totalData.valuearr[y][i] = el + nextEl;
-                        totalData.valuearr[atPresentY][atPresentX] = 0;
-                        atPresentX = i;
+                    if (nextEl === el) {
+                        totalData.valuearr[y][i + 1] = el + nextEl;
+                        totalData.valuearr[y][i] = 0;
                     }
                 }
             }
@@ -318,3 +324,4 @@ function judgeAndAdd(mod) {
     }
     // console.log(totalData.valuearr);
 }
+
